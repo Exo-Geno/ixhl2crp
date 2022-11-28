65d532e234a676b012ed0fa8206dde64fede7ed9
Jeanleef <81259687+Exo-Geno@users.noreply.github.com>
2022-11-28 18:49:01 +0100

    Add files via upload

---
+++ storage.lua
@@ -0,0 +1,12 @@
+local PLUGIN = PLUGIN
+
+PLUGIN.name = "New Storage Containers"
+PLUGIN.desc = "Registers new containers."
+PLUGIN.author = "Apsys"
+
+ix.container.Register("models/props_junk/wood_crate002a.mdl", {
+    name = "A long birch wood crate",
+    description = "A simple wooden drobe.",
+    width = 12,
+    height = 7,
+})
---
+++ thirdpersonbind.lua
@@ -0,0 +1,15 @@
+
+PLUGIN.name         = 'Third-person view bind'
+PLUGIN.description  = 'Allows you toggle third-person view using bind'
+PLUGIN.author       = 'Bilwin'
+PLUGIN.button       = KEY_F4
+
+if CLIENT then
+    local Cooldown
+    function PLUGIN:PlayerButtonDown(_, button)
+        if button == self.button && (Cooldown || 0) < CurTime() then
+            RunConsoleCommand('ix_togglethirdperson')
+            Cooldown = CurTime() + 0.1
+        end
+    end
+end
