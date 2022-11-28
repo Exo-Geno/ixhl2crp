96d4c31d1729e77b975b1ded377c67dad813bef5
Jeanleef <81259687+Exo-Geno@users.noreply.github.com>
2022-11-28 18:52:18 +0100

    Delete moneymodel.lua

--- moneymodel.lua
+++
@@ -1,7 +0,0 @@
-local PLUGIN = PLUGIN
-
-PLUGIN.name = "MoneyChangeModel"
-PLUGIN.author = "Usuariozombie"
-PLUGIN.description = "Simple, it just change tokens model without editing helix plugins."
-
-ix.currency.model = "models/bioshockinfinite/hext_coin.mdl"
